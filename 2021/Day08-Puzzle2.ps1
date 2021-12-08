0fa5e093463c2c1f07681b5ae9ec54c2466592fa
Nathan Ziehnert <nathan@z-nerd.com>
2021-12-08 09:53:22 -0700

    Update Day08-Puzzle2.ps1

--- 2021/Day08-Puzzle2.ps1
+++ 2021/Day08-Puzzle2.ps1
@@ -1,28 +1,29 @@
 $puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\08\input.txt'
 
-$fourDigitSegments = @()
+$sum = 0
 foreach($segment in $puzzleInput)
 {
     $split = ($segment -Split ' \| ')
     $numbers = $split[1].Split(" ")
 
-    $One = $split[0].Split(" ") | Where-Object {$_.ToCharArray().Count -eq 2}
-    $Four = $split[0].Split(" ") | Where-Object {$_.ToCharArray().Count -eq 4}
-    $Seven = $split[0].Split(" ") | Where-Object {$_.ToCharArray().Count -eq 3}
-    $Eight = $split[0].Split(" ") | Where-Object {$_.ToCharArray().Count -eq 7}
     $notes = $split[0].Split(" ")
+    $One = ($notes.Where({$_.ToCharArray().Count -eq 2})).ToCharArray() | Sort-Object
+    $Four = ($notes.Where({$_.ToCharArray().Count -eq 4})).ToCharArray() | Sort-Object
+    $Seven = ($notes.Where({$_.ToCharArray().Count -eq 3})).ToCharArray() | Sort-Object
+    $Eight = ($notes.Where({$_.ToCharArray().Count -eq 7})).ToCharArray() | Sort-Object
+    
 
     $displayHash = @{
-        1 = $One.ToCharArray() | Sort-Object
-        2 = ($notes | Where-Object {$_.ToCharArray().Count -eq 5 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 1 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 2 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 2}).ToCharArray() | Sort-Object
-        3 = ($notes | Where-Object {$_.ToCharArray().Count -eq 5 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 2 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 3 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 3}).ToCharArray() | Sort-Object
-        4 = $Four.ToCharArray() | Sort-Object
-        5 = ($notes | Where-Object {$_.ToCharArray().Count -eq 5 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 1 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 3 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 2}).ToCharArray() | Sort-Object
-        6 = ($notes | Where-Object {$_.ToCharArray().Count -eq 6 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 1 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 3 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 2}).ToCharArray() | Sort-Object
-        7 = $Seven.ToCharArray() | Sort-Object
-        8 = $Eight.ToCharArray() | Sort-Object
-        9 = ($notes | Where-Object {$_.ToCharArray().Count -eq 6 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 2 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 4 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 3}).ToCharArray() | Sort-Object
-        0 = ($notes | Where-Object {$_.ToCharArray().Count -eq 6 -and ($_.ToCharArray() | ?{$One.ToCharArray() -contains $_}).Count -eq 2 -and ($_.ToCharArray() | ?{$Four.ToCharArray() -contains $_}).Count -eq 3 -and ($_.ToCharArray() | ?{$Seven.ToCharArray() -contains $_}).Count -eq 3}).ToCharArray() | Sort-Object
+        1 = $One
+        2 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 5 -and ($ca | ?{$One -contains $_}).Count -eq 1 -and ($ca | ?{$Four -contains $_}).Count -eq 2 -and ($ca | ?{$Seven -contains $_}).Count -eq 2)})).ToCharArray() | Sort-Object
+        3 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 5 -and ($ca | ?{$One -contains $_}).Count -eq 2 -and ($ca | ?{$Four -contains $_}).Count -eq 3 -and ($ca | ?{$Seven -contains $_}).Count -eq 3)})).ToCharArray() | Sort-Object
+        4 = $Four
+        5 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 5 -and ($ca | ?{$One -contains $_}).Count -eq 1 -and ($ca | ?{$Four -contains $_}).Count -eq 3 -and ($ca | ?{$Seven -contains $_}).Count -eq 2)})).ToCharArray() | Sort-Object
+        6 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 6 -and ($ca | ?{$One -contains $_}).Count -eq 1 -and ($ca | ?{$Four -contains $_}).Count -eq 3 -and ($ca | ?{$Seven -contains $_}).Count -eq 2)})).ToCharArray() | Sort-Object
+        7 = $Seven
+        8 = $Eight
+        9 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 6 -and ($ca | ?{$One -contains $_}).Count -eq 2 -and ($ca | ?{$Four -contains $_}).Count -eq 4 -and ($ca | ?{$Seven -contains $_}).Count -eq 3)})).ToCharArray() | Sort-Object
+        0 = ($notes.Where({$ca = $_.ToCharArray(); ($ca.Count -eq 6 -and ($ca | ?{$One -contains $_}).Count -eq 2 -and ($ca | ?{$Four -contains $_}).Count -eq 3 -and ($ca | ?{$Seven -contains $_}).Count -eq 3)})).ToCharArray() | Sort-Object
     }
 
     $fourDigitSegment = @{
@@ -30,7 +31,6 @@ foreach($segment in $puzzleInput)
         2 = $numbers[1].ToCharArray() | Sort-Object
         3 = $numbers[2].ToCharArray() | Sort-Object
         4 = $numbers[3].ToCharArray() | Sort-Object
-        Value = 0
     }
 
     for($i = 0; $i -lt 4; $i++)
@@ -40,12 +40,10 @@ foreach($segment in $puzzleInput)
             if([string]($fourDigitSegment[$i+1]-join'') -eq [string]($displayHash[$key]-join''))
             {
                 $fourDigitSegment[$i+1] = $key
+                break
             }
         }
     }
-    $fourDigitSegment.Value = [int]::Parse(@($fourDigitSegment[1],$fourDigitSegment[2],$fourDigitSegment[3],$fourDigitSegment[4])-join'')
-    $fourDigitSegments += $fourDigitSegment
+    $sum += [int]::Parse($fourDigitSegment[1..4]-join'')
 }
-$sum = 0
-$fourDigitSegments.Value | %{$sum += $_}
 $sum
