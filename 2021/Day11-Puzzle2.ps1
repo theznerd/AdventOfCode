50dcfa590321ac9d90ae3161b600fb54c01d48e8
Nathan Ziehnert <nathan@z-nerd.com>
2021-12-11 14:48:55 -0700

    Create Day11-Puzzle2.ps1

---
+++ 2021/Day11-Puzzle2.ps1
@@ -0,0 +1,66 @@
+$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\11\input.txt'
+$puzzleX = $puzzleInput[0].ToCharArray().Count
+$puzzleY = $puzzleInput.Count
+$octopuses = New-Object 'object[,]' $puzzleX,$puzzleY
+
+#Build map
+for($y = 0; $y -lt $puzzleY; $y++)
+{
+    for($x = 0; $x -lt $puzzleX; $x++)
+    {
+        $octopuses[$x,$y] = @{
+            power = $([int]::Parse(($puzzleInput[$y].ToCharArray())[$x]))
+            flashed = $false
+        }
+    }
+}
+
+function IncreaseEnergy($octopusArray)
+{
+    foreach($octopus in $octopusArray)
+    {
+        $octopus.power++
+    }
+}
+
+function FlashOctopuses($octopusArray)
+{
+    for($y = 0; $y -lt $puzzleY; $y++)
+    {
+        for($x = 0; $x -lt $puzzleX; $x++)
+        {
+            if(!$octopusArray[$x,$y].flashed -and $octopusArray[$x,$y].power -gt 9)
+            {
+                $octopusArray[$x,$y].flashed = $true
+                if($x+1 -lt $puzzleX){($octopusArray[($x+1),$y]).power++}
+                if($x+1 -lt $puzzleX -and $y+1 -lt $puzzleY){($octopusArray[($x+1),($y+1)]).power++}
+                if($y+1 -lt $puzzleY){($octopusArray[$x,($y+1)]).power++}
+                if($y+1 -lt $puzzleY -and $x-1 -ge 0){($octopusArray[($x-1),($y+1)]).power++}
+                if($x-1 -ge 0){($octopusArray[($x-1),$y]).power++}
+                if($y-1 -ge 0 -and $x-1 -ge 0){($octopusArray[($x-1),($y-1)]).power++}
+                if($y-1 -ge 0){($octopusArray[$x,($y-1)]).power++}
+                if($x+1 -lt $puzzleX -and $y-1 -ge 0){($octopusArray[($x+1),($y-1)]).power++}
+                FlashOctopuses $octopusArray
+            }
+        }
+    }
+}
+
+function ResetFlashes($octopusArray)
+{
+    foreach($octopus in $octopusArray.Where({$_.power -gt 9}))
+    {
+        $octopus.flashed = $false
+        $octopus.power = 0
+    }
+}
+
+$step = 0
+do
+{
+    $step++
+    [void](ResetFlashes $octopuses)
+    IncreaseEnergy $octopuses
+    FlashOctopuses $octopuses
+}until($octopuses.Count -eq $octopuses.Where({$_.flashed -eq $true}).Count)
+$step
