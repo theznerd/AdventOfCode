8a02d033d53e906fe8214313d4969b21f950aa62
Nathan Ziehnert <nathan@z-nerd.com>
2021-12-12 19:57:48 -0700

    Add files via upload

---
+++ 2021/Day12-Part1.ps1
@@ -0,0 +1,76 @@
+﻿$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\input.txt'
+#$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\test-input.txt'
+#$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\simple-input.txt'
+
+# Get a list of caves (probably can be improved)
+$caves = @()
+foreach($line in $puzzleInput)
+{
+    $caves += $line.Split("-")
+}
+$caves = $caves | select -Unique
+
+# Build the initial cave map
+$caveMap = @()
+foreach($cave in $caves)
+{ 
+    $caveMap += [pscustomobject]@{
+        Name = $cave
+        Size = if($cave -cmatch "^[A-Z]*$"){"large"}else{"small"}
+        Neighbors = @($puzzleInput.Where({$_ -like "$cave-*" -or $_ -like "*-$cave"}).Replace("-$cave","").Replace("$cave-",""))
+    }
+}
+
+# Swap neighbors for object model
+for($i = 0; $i -lt $caveMap.Count; $i++)
+{
+    for($j = 0; $j -lt $caveMap[$i].Neighbors.Count; $j++)
+    {
+        $caveMap[$i].Neighbors[$j] = $caveMap.Where({$_.Name -eq $caveMap[$i].Neighbors[$j]})[0]
+    }
+}
+
+function FindPaths($start, $pathVisitedNeighbors, $path)
+{
+    $paths = @()
+    $path = if([string]::IsNullOrEmpty($path)){$start.Name}else{$path}
+    $pathVisitedNeighbors += $start
+    foreach($neighbor in $start.Neighbors)
+    {
+        $pathOriginal = $path
+        $pathVisitedNeighborsOriginal = @($pathVisitedNeighbors)
+        if($neighbor.Name -eq "end")
+        {
+            $path += ",$($neighbor.Name)"
+            $paths += $path
+            $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+            $path = $pathOriginal
+            continue
+        }
+        elseif($neighbor.Size -eq "large" -or $pathVisitedNeighbors -notcontains $neighbor)
+        {
+            $path += ",$($neighbor.Name)"
+            $paths += FindPaths $neighbor $pathVisitedNeighbors $path
+            $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+            $path = $pathOriginal
+        }
+    }
+    return $paths
+}
+(FindPaths $caveMap.Where({$_.Name -eq "start"})).Count
+
+
+<#
+function FindPaths($start, $visitedCaves)
+{
+    $visitedCaves += $start
+    foreach($neighbor in $start.Neighbors)
+    {
+        if($visitedCaves.Size -eq "large" -or $visitedCaves -notcontains $neighbor)
+        {
+            FindPaths $neighbor $visitedCaves
+        }
+    }
+}
+FindPaths $caveMap.Where({$_.Name -eq "start"})
+#>
---
+++ 2021/Day12-Part2.ps1
@@ -0,0 +1,88 @@
+﻿$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\input.txt'
+#$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\test-input.txt'
+#$puzzleInput = Get-Content 'C:\Users\Nathan Ziehnert\OneDrive - Z-Nerd\Documents\AOC\2021\12\simple-input.txt'
+
+# Get a list of caves (probably can be improved)
+$caves = @()
+foreach($line in $puzzleInput)
+{
+    $caves += $line.Split("-")
+}
+$caves = $caves | select -Unique
+
+# Build the initial cave map
+$caveMap = @()
+foreach($cave in $caves)
+{ 
+    $caveMap += [pscustomobject]@{
+        Name = $cave
+        Size = if($cave -cmatch "^[A-Z]*$"){"large"}else{"small"}
+        Neighbors = @($puzzleInput.Where({$_ -like "$cave-*" -or $_ -like "*-$cave"}).Replace("-$cave","").Replace("$cave-",""))
+    }
+}
+
+# Swap neighbors for object model
+for($i = 0; $i -lt $caveMap.Count; $i++)
+{
+    for($j = 0; $j -lt $caveMap[$i].Neighbors.Count; $j++)
+    {
+        $caveMap[$i].Neighbors[$j] = $caveMap.Where({$_.Name -eq $caveMap[$i].Neighbors[$j]})[0]
+    }
+}
+
+function FindPaths($start, $pathVisitedNeighbors, $path, $smallCaveExtraVisit)
+{
+    $paths = @()
+    $path = if([string]::IsNullOrEmpty($path)){$start.Name}else{$path}
+    $pathVisitedNeighbors += $start
+    $smallCaveExtraVisit = if($smallCaveExtraVisit -eq $null){$false}else{$smallCaveExtraVisit}
+    foreach($neighbor in $start.Neighbors)
+    {
+        $pathOriginal = $path
+        $pathVisitedNeighborsOriginal = @($pathVisitedNeighbors)
+        $smallCaveExtraVisitOriginal = $smallCaveExtraVisit
+        if($neighbor.Name -eq "end")
+        {
+            $path += ",$($neighbor.Name)"
+            $paths += $path
+            $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+            $path = $pathOriginal
+            $smallCaveExtraVisit = $smallCaveExtraVisitOriginal
+            continue
+        }
+        elseif($neighbor.Name -eq "start")
+        {
+            continue
+        }
+        elseif($neighbor.Size -eq "large")
+        {
+            $path += ",$($neighbor.Name)"
+            $paths += FindPaths $neighbor $pathVisitedNeighbors $path $smallCaveExtraVisit
+            $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+            $smallCaveExtraVisit = $smallCaveExtraVisitOriginal
+            $path = $pathOriginal
+        }
+        else
+        {
+            if($pathVisitedNeighbors -contains $neighbor -and !$smallCaveExtraVisit)
+            {
+                $smallCaveExtraVisit = $true
+                $path += ",$($neighbor.Name)"
+                $paths += FindPaths $neighbor $pathVisitedNeighbors $path $smallCaveExtraVisit
+                $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+                $smallCaveExtraVisit = $smallCaveExtraVisitOriginal
+                $path = $pathOriginal
+            }
+            elseif($pathVisitedNeighbors -notcontains $neighbor)
+            {
+                $path += ",$($neighbor.Name)"
+                $paths += FindPaths $neighbor $pathVisitedNeighbors $path $smallCaveExtraVisit
+                $pathVisitedNeighbors = $pathVisitedNeighborsOriginal
+                $smallCaveExtraVisit = $smallCaveExtraVisitOriginal
+                $path = $pathOriginal
+            }
+        }
+    }
+    return $paths
+}
+(FindPaths $caveMap.Where({$_.Name -eq "start"})).Count
