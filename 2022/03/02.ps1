3ce8fd9afb973b97cd96f0be887a62aee88a93de
Nathan Ziehnert <nathan@z-nerd.com>
2022-12-02 22:34:04 -0700

    -join wasn't necessary lol

--- 2022/03/02.ps1
+++ 2022/03/02.ps1
@@ -7,7 +7,7 @@ for($r = 0; $r -lt $input.count; $r += 3)
     $bagTwo = $input[($r+1)]
     $bagThree = $input[($r+2)]
 
-    $firstIntersect = [System.Linq.Enumerable]::Intersect($bagOne,$bagTwo) -join ''
+    $firstIntersect = [System.Linq.Enumerable]::Intersect($bagOne,$bagTwo)
     $priority = [System.Linq.Enumerable]::Intersect($firstIntersect,$bagThree)
 
     if($priority -cmatch "[A-Z]"){
