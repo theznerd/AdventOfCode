c7c475ac0c64fba7eb5bb6acdc287e3b8bc0a370
Nathan Ziehnert <nathan@z-nerd.com>
2022-12-03 23:51:15 -0700

    added some comments

--- 2022/04/02.ps1
+++ 2022/04/02.ps1
@@ -6,6 +6,10 @@ foreach($pair in $input)
     $elfoneLow, $elfoneHigh, $elftwoLow, $elftwoHigh = $pair.Split(',').Split('-')
     [int[]]$elfone = $elfoneLow..$elfoneHigh
     [int[]]$elftwo = $elftwoLow..$elftwoHigh
+    ## this ends up being slower than it needs to be
+    ## because it's accounting for situations where
+    ## an elf could be assigned 1,2,5 rather than a
+    ## range (1,2,3,4,5)
     if(([System.Linq.Enumerable]::Intersect($elfone,$elftwo)).Count -gt 0){$overlaps++}
 }
 $overlaps
