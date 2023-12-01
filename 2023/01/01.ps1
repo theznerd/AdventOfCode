598172d3d7f1b8926ae992e35f4c05f69604ecf5
Nathan Ziehnert <nathan@z-nerd.com>
2023-11-30 23:58:51 -0700

    Already don't like day 1

---
+++ 2023/01/01.ps1
@@ -0,0 +1,10 @@
+#$puzzleInput = Get-Content $PSScriptRoot\example.txt
+$puzzleInput = Get-Content $PSScriptRoot\input.txt
+
+$calibrationValue = 0
+foreach($line in $puzzleInput){
+    $regx = [regex]'(\d)'
+    $numbers = $regx.Matches($line)
+    $calibrationValue += [int]"$($numbers[0].Value)$($numbers[-1].Value)"
+}
+$calibrationValue
---
+++ 2023/01/02.ps1
@@ -0,0 +1,61 @@
+#$puzzleInput = Get-Content $PSScriptRoot\example2.txt
+$puzzleInput = Get-Content $PSScriptRoot\input.txt
+
+enum singleDigits {
+    zero
+    one
+    two
+    three
+    four
+    five
+    six
+    seven
+    eight
+    nine
+}
+
+# This doesn't do what we expect... because all the numbers are valid threeight should be 38 not 3ight
+<#
+function Update-Digits ($string) {
+    $regx = '(one|two|three|four|five|six|seven|eight|nine)'
+    $newString = $string -replace $regx, {"$([int][singleDigits]::$($_))"}
+    if($newString -eq $string){return $newString}
+    else{Update-Digits $newString}
+}
+#>
+
+# Only a single character would overlap, and we only need to replace the first and last instance
+function Update-Digits ($string) {
+    $regx = '(one|two|three|four|five|six|seven|eight|nine)'
+    $newString = $string -replace $regx, {"$([int][singleDigits]::$($_))$_"}
+    
+    ## Ugh... reverse matching? I don't like this solution...
+    $words = "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    $lastIndex = -1
+    $lastLength = 0
+    $lastValue = ""
+    foreach($word in $words)
+    {
+        $regx = [regex]"$word"
+        $m = $regx.Matches($newString)
+        if($m -and $m[-1].Index -gt $lastIndex){$lastIndex = $m[-1].Index; $lastLength = $m[-1].Length; $lastValue = $m[-1].Value}
+    }
+    if($lastIndex -eq -1){
+        return $newString
+    }
+    if($lastIndex -eq 0){
+        return "$([int][singleDigits]::$lastValue)$($newString[($lastLength)..($newString.Length)] -join '')"
+    }
+    if($lastIndex -gt 0){
+        return "$($newString[0..$lastIndex] -join '')$([int][singleDigits]::$lastValue)$($newString[($lastIndex + $lastLength)..($newString.Length)] -join '')"
+    }
+}
+
+$calibrationValue = 0
+foreach($line in $puzzleInput){
+    $rline = (Update-Digits $line)
+    $regx = [regex]'(\d)'
+    $numbers = $regx.Matches($rline)
+    $calibrationValue += [int]"$($numbers[0].Value)$($numbers[-1].Value)"
+}
+$calibrationValue
