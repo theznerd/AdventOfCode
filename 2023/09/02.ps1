99b54163307c6a1dc123e3f105e9489737afb3c6
Nathan Ziehnert <nathan@z-nerd.com>
2023-12-09 21:32:19 -0700

    Little late to getting to the puzzle... but it's done

---
+++ 2023/09/02.ps1
@@ -0,0 +1,45 @@
+#$puzzleInput = Get-Content $PSScriptRoot\example.txt
+$puzzleInput = Get-Content $PSScriptRoot\input.txt
+
+function Get-NextLevel ($history){
+    $nextLevel = [System.Collections.Generic.List[int]]::new()
+    for($i = 0; $i -lt ($history.Count - 1); $i++)
+    {
+        $nextLevel.Add(($history[($i+1)] - $history[($i)]))
+    }
+    return ,$nextLevel
+}
+
+function Get-ValueMap ($history, $valueMap = [System.Collections.Generic.List[object]]::new())
+{
+    $valueMap.Add($history)
+    $nextLevel = Get-NextLevel($history)
+    if([System.Linq.Enumerable]::Count([System.Linq.Enumerable]::Distinct($nextLevel)) -eq 1 -and $nextLevel[0] -eq 0)
+    {
+        $valueMap.Add($nextLevel)
+        return ,$valueMap
+    }
+    else {
+        return ,(Get-ValueMap $nextLevel $valueMap)
+    }
+}
+
+function Get-NextValue ($valueMap)
+{
+    $nextValue = 0
+    for($l = ($valueMap.Count - 2); $l -ge 0; $l--)
+    {
+        $nextValue += $valueMap[$l][-1]
+    }
+    $nextValue
+}
+
+$sum = 0
+for($i = 0; $i -lt $puzzleInput.Count; $i++)
+{
+    $history = [int[]]($puzzleInput[$i] -split " ")
+    $history = $history[($history.Count - 1)..0]
+    $vm = Get-ValueMap $history
+    $sum += Get-NextValue $vm
+}
+$sum
